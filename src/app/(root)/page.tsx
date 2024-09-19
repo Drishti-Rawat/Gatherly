2a1d29ac58f16488c574a397534d10dc73fe2f4c
Drishti <drishtirwt019@gmail.com>
2024-09-19 19:03:10 +0530

    project setup

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Gatherly</h1>
+    
+
+    </main>
+  );
+}
