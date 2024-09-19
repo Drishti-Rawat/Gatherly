23f0a87ba90659c2ef1166d45c2427ed7e735430
Drishti <drishtirwt019@gmail.com>
2024-09-19 23:20:58 +0530

    footer

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,10 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col p-5  '>
+      <Link className='flex items-center ' href={'/'}>
+    <Image src={"/assets/images/logo.png"} width={50} height={50} alt={"logo"}/>
+    <h2 className='text-2xl font-bold  gradient-text'>Gatherly</h2>
+    </Link>
+
+    <p className='text-sm'>&copy; 2024 Gatherly. All Rights Reserved.</p>
+      </div>
+
       
-    </div>
+    </footer>
   )
 }
 
