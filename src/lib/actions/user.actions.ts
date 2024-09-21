3164a05cb27cdf1c9c266372fd64e0fedfec8499
Drishti <drishtirwt019@gmail.com>
2024-09-21 21:18:18 +0530

    db model setup(update , delte,creat user)

---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,85 @@
+'use server'
+import { CreateUserParams, UpdateUserParams } from "@/types"
+import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
+import User from "../database/models/user.model"
+import { json } from "stream/consumers"
+import { error } from "console"
+import { revalidatePath } from "next/cache"
+import Order from "../database/models/order.model"
+import Event from "../database/models/event.model"
+
+export const createUser = async (user:CreateUserParams) => {
+    try {
+
+        await connectToDatabase();
+
+        const newUser = await User.create(user);
+        return JSON.parse(JSON.stringify(newUser))
+        
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export const getUserById = async (userId:string) => {
+    try {
+
+        await connectToDatabase();
+
+        const user = await User.findOne({clerkId:userId});
+        return JSON.parse(JSON.stringify(user))
+        
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function updateUser(clerkId:string,user:UpdateUserParams) {
+
+    try {
+        
+        await connectToDatabase();
+
+        const updatedUser = await User.findOneAndUpdate({clerkId:clerkId},user, {new:true});
+        if(!updateUser){
+            throw new Error('User not found')
+
+        }
+        return JSON.parse(JSON.stringify(updatedUser))
+    } catch (error) {
+        handleError(error)
+    }
+    
+}
+
+export async function deleteUser(clerkId:string) {
+    try {
+        await connectToDatabase();
+
+        // find user to delete
+        const userToDelete = await User.findOne({clerkId:clerkId});
+        if(!userToDelete){
+            throw new Error('User not found')
+        }
+        // Unlink relationships
+    await Promise.all([
+        // Update the 'events' collection to remove references to the user
+        Event.updateMany(
+          { _id: { $in: userToDelete.events } },
+          { $pull: { organizer: userToDelete._id } }
+        ),
+  
+        // Update the 'orders' collection to remove references to the user
+        Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+      ])
+  
+      // Delete user
+      const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+      revalidatePath('/')
+  
+      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+    } catch (error) {
+        handleError(error)
+    }
+}
---
+++ src/lib/database/models/order.model.ts
@@ -0,0 +1,34 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IOrder {
+    createdAt: Date,
+    stripeId: string,
+    totalAmount: string,
+    event: {
+        _id: string
+        title: string
+    },
+    buyer:{
+        _id: string
+        username: string
+        firstName: string
+        lastName: string
+    }
+}
+
+const OrderSchema = new Schema({
+    createdAt :{type:Date,default:Date.now},
+    stripeId: { type: String, required: true,unique: true },
+    totalAmount : { type: String },
+    event:{
+        type:Schema.Types.ObjectId,
+        ref:"Event"
+    },
+    buyer:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+})
+
+const Order = models.Order || model("Order", OrderSchema);
+export default Order
