5957a84a28aaef0a162af06dea0d237e089871ba
Drishti <drishtirwt019@gmail.com>
2024-09-21 23:06:51 +0530

    .

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -2,23 +2,19 @@ import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI;
 
-const cached =(global as any).mongoose|| {conn:null, promise:null};
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connectToDatabase = async ()=>{
-    if(cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    if(!MONGO_URI){
-        throw new Error("Please define the MONGO_URI environment variable inside .env.local")
-    }
-   cached.promise=cached.promise || mongoose.connect(MONGO_URI,{
-    dbName:"Gatherly",
-    bufferCommands:false
-   })
+  if(!MONGO_URI) throw new Error('MONGODB_URI is missing');
 
-   cached.conn=await cached.promise
-   console.log("database connected successfully")
-   return cached.conn;
+  cached.promise = cached.promise || mongoose.connect(MONGO_URI, {
+    dbName: 'Gatherly1',
+    bufferCommands: false,
+  })
 
-   
-   
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
