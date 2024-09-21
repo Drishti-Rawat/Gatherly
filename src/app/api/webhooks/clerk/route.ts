d45487f44202a733c8d14e6620034b1cf4fea628
Drishti <drishtirwt019@gmail.com>
2024-09-21 21:20:57 +0530

    .

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -52,6 +52,7 @@ export async function POST(req: Request) {
   // Do something with the payload
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
+  console.log(id)
   const eventType = evt.type;
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
