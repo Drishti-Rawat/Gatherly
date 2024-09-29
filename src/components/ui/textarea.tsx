96fc236b2fc24fb48d03740d95b8d4cbe5038db8
Drishti <drishtirwt019@gmail.com>
2024-09-30 00:02:11 +0530

    .

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -3,9 +3,11 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  // Future custom props can be added here
+  }
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
